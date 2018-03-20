5153ded8ccc0dbbcdcdf99c9e93fded48012a850
Nick Wilson <logikfx@gmail.com>
2018-03-20 13:11:09 -0400

    Made Sphere class, still need intersection methods

---
+++ Sphere.js
@@ -0,0 +1,12 @@
+/* Sphere.js */
+
+class Sphere {
+    constructor (center, radius, material) {
+        this.center = center;
+        this.radius = radius;
+        this.material = material;
+    }
+
+// need intersection methods
+
+}
