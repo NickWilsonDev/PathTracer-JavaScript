d655dff373c3233a6504a58a9cef7bcbe04767ce
Nick Wilson <logikfx@gmail.com>
2018-03-20 16:01:30 -0400

    added some more to Sphere class

--- Sphere.js
+++ Sphere.js
@@ -5,8 +5,16 @@ class Sphere {
         this.center = center;
         this.radius = radius;
         this.material = material;
+        this.radiusSquared = this.radius * this.radius;
     }
 
 // need intersection methods
+    intersection(ray) {
+    // calculate values used for finding determinant
 
+    // only hits if determinant in 0 or greater
+
+
+    //return distance to hitpoint?
+    }
 }
